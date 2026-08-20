0483bf7f228051ef7b9b76180acff9535df5ea89
ADM Official <admofficialhq@gmail.com>
2026-08-21 01:54:13 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,18 @@
-require('dotenv').config();
+const path = require('path');
+
+const port = Number(process.env.PORT || 3000);
+
+const sessionDir = process.env.SESSION_DIR
+    || path.join(process.cwd(), 'sessions');
+
+const dbPath = process.env.DB_PATH
+    || path.join(process.cwd(), 'data', 'adm.db');
+
+const apiKey = process.env.API_KEY || '';
 
 module.exports = {
-    port: Number(process.env.PORT || 8080),
-    apiKey: process.env.API_KEY || '',
-    sessionDir: process.env.SESSION_DIR || './sessions'
+    port,
+    sessionDir,
+    dbPath,
+    apiKey
 };
